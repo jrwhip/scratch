431bbc34f8ebc9b75a7ec86aab3916d75be97431
Jared Whipple <jaredwhipple@utah.gov>
2023-03-22 17:14:42 -0600

    Simplify Store

--- store.service.ts
+++ store.service.ts
@@ -12,88 +12,54 @@ const initialState = new State();
   providedIn: 'root',
 })
 export class StoreService {
-  #store$: BehaviorSubject<State>;
+  // Store the state in a BehaviorSubject, so subscribers can receive the latest state updates.
+  protected storeSubject$: BehaviorSubject<State> = new BehaviorSubject<State>(initialState);
 
-  #storeUpdates$: Subject<Partial<State>>;
+  // Exports the entire store as an observable, so subscribers can receive the entire state.
+  store$: Observable<State> = this.storeSubject$.asObservable();
 
-  store$: Observable<State>;
+  // Subject for updating the store with partial state updates.
+  private storeUpdates$: Subject<Partial<State>> = new Subject<Partial<State>>();
 
-  // isLoading$: Observable<boolean>;
-
-  // currentUser$: Observable<CurrentUser | null>;
-
-  get isLoading$(): Observable<State['isLoading']> {
-    return this.store$.pipe(distinctUntilKeyChanged('isLoading'));
+  // Observable for the 'isLoading' state property.
+  get isLoading$(): Observable<boolean> {
+    return this.storeSubject$.pipe(
+      distinctUntilKeyChanged('isLoading'),
+      map((state) => state?.isLoading),
+    );
   }
 
-  get currentUser$(): Observable<boolean> {
-    return this.store$.pipe(distinctUntilKeyChanged('currentUser'));
-  }
-
-    get isLoading$(): Observable<boolean> {
-    return this.store$.pipe(map((state) => state.isLoading), distinctUntilChanged());
-  }
-
-  get rolesList$(): Observable<{ [key: number]: string }> {
-    return this.store$.pipe(map((state) => state.rolesList), distinctUntilChanged());
-  }
-
-  get authUser$(): Observable<AuthUser> {
-    return this.store$.pipe(map((state) => state.authUser), distinctUntilChanged());
-  }
-
+  // Observable for the 'currentUser' state property.
   get currentUser$(): Observable<CurrentUser | null> {
-    return this.store$.pipe(map((state) => state.currentUser), distinctUntilChanged());
-  }
-
-  get currentUser$(): Observable<State[keyof State] | null> {
-    return this.#store$.pipe(
+    return this.storeSubject$.pipe(
       distinctUntilKeyChanged('currentUser'),
       map((state) => state?.currentUser),
     );
   }
 
-  get user$(): Observable<Users> {
-    return this.store$.pipe(map((state) => state.user), distinctUntilChanged());
-  }
-
-  get vmViewMtpRoleAccess$(): Observable<VmViewMtpRoleAccess> {
-    return this.store$.pipe(map((state) => state.vmViewMtpRoleAccess), distinctUntilChanged());
-  }
-
   constructor() {
-    this.#store$ = new BehaviorSubject(initialState);
-    this.store$ = this.#store$.asObservable();
-    this.#storeUpdates$ = new Subject();
-
-    // this.isLoading$ = this.#store$.pipe(
-    //   map((store) => store.isLoading),
-    //   distinctUntilChanged(),
-    // );
-
-    // this.currentUser$ = this.#store$.pipe(
-    //   map((store) => store.currentUser),
-    //   distinctUntilChanged(),
-    // );
-
-    this.#storeUpdates$
+    // Combine partial state updates with the current state to produce a new state.
+    this.storeUpdates$
       .pipe(scan((acc, curr) => ({ ...acc, ...curr }), initialState))
-      .subscribe(this.#store$);
+      .subscribe(this.storeSubject$);
   }
 
+  // Update the store with a partial state update.
   updateStore(storeUpdate: Partial<State>): void {
-    this.#storeUpdates$.next(storeUpdate);
+    this.storeUpdates$.next(storeUpdate);
   }
 
+  // Query the store for a single key and return an observable of that key's value.
   queryStoreSingleKey(keyString: keyof State): Observable<State[keyof State]> {
-    return this.#store$.pipe(
+    return this.storeSubject$.pipe(
       distinctUntilKeyChanged(keyString),
       map((key) => key?.[keyString]),
     );
   }
 
+  // Query the store for multiple keys and return an observable of an object with the specified keys.
   queryStoreMultiKeys(keyArr: (keyof State)[]): Observable<Partial<State>> {
-    return this.#store$.pipe(
+    return this.storeSubject$.pipe(
       distinctUntilChanged((prev, curr) => !keyArr.some((key) => prev[key] !== curr[key])),
       map((val) => {
         const newKey = [...keyArr];
