dc40e6e3d74e520a09b18c1898016e5069f75842
Jared Whipple <jaredwhipple@utah.gov>
2023-03-24 10:29:10 -0600

    kina working example

--- scratch-1.ts
+++ scratch-1.ts
@@ -37,3 +37,49 @@ this.payments$ = combineLatest([
     distinctUntilChanged(),
     shareReplay()
 );
+
+// hey
+
+paymentPercentage$ = this.paymentForm
+.get('paymentPercentage')!
+.valueChanges.pipe(
+    startWith(this.paymentForm.get('paymentPercentage')!.value ?? 0)
+);
+
+this.payments$ = combineLatest([
+    this.baseRate$,
+    this.totalChildren$,
+    this.paymentPercentage$
+]).pipe(
+    map(([baseRate, totalChildren, paymentPercentage]) => {
+    const subTotal = baseRate * totalChildren;
+    const partTimeEligiblePayment = this.calculatePercentage(subTotal, 50);
+    const paymentPercentageNum = () => {
+        console.log(paymentPercentage);
+        const num = parseInt(paymentPercentage, 10) || 0;
+        console.log(num);
+        if (num < 0) {
+        this.paymentForm.get('paymentPercentage')?.setValue(0);
+        return 0;
+        } else if (num > 100) {
+        this.paymentForm.get('paymentPercentage')?.setValue(100);
+        return 100;
+        } else {
+        return num;
+        }
+    };
+    const percentagePayment = this.calculatePercentage(
+        subTotal,
+        paymentPercentageNum()
+    );
+    return {
+        subTotal,
+        baseRate,
+        fullTimeEligiblePayment: subTotal,
+        partTimeEligiblePayment,
+        percentagePayment
+    };
+    }),
+    distinctUntilChanged(),
+    shareReplay()
+);
