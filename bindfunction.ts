a221cdfe080c6e309de7099f35c0668b4cb7039c
Jared Whipple <jared.whipple@stgconsulting.com>
2023-05-16 02:12:43 -0600

    update bind function

--- bindfunction.ts
+++ bindfunction.ts
@@ -4,7 +4,8 @@ export function bindOnClickFunction<T>(component: T, colDefs: ColDef[]): ColDef[
   return colDefs.map(colDef => {
     if (colDef.cellRendererParams && colDef.cellRendererParams.buttons) {
       colDef.cellRendererParams.buttons = colDef.cellRendererParams.buttons.map(button => {
-        const boundFunction = component[button.functionName as keyof T].bind(component, button) as (...args: any[]) => void;
+        const method = component[button.functionName as keyof T] as (...args: any[]) => void;
+        const boundFunction = method.bind(component, button);
         return {
           ...button,
           onClick: boundFunction,
