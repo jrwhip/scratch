9ef705b47d50901b987c8d2fde6e8aceb9b2055a
Jared Whipple <jared.whipple@stgconsulting.com>
2023-05-23 10:38:46 -0600

    This

--- fuck-bing.ts
+++ fuck-bing.ts
@@ -1,47 +1,44 @@
-<div class="container">
-  <div class="card p-4 mt-3">
-    <div class="d-flex flex-column align-items-center">
-      <h1 class="text-center py-4">Performance Exam Edit Versions</h1>
-      <h4 class="text-muted">
-        NOTE: This will only show versions of performance exams associated to rating forms. Thus,
-        some version history may be missing.
-      </h4>
-    </div>
+<div class="container d-flex justify-content-center">
+  <div class="card p-4 mt-3 shadow-sm" style="max-width: 800px;">
+    <h1 class="text-center py-4 fw-bold">Performance Exam Edit Versions</h1>
+    <p class="text-muted text-center fw-light mb-4">
+      NOTE: This will only show versions of performance exams associated to rating forms. Thus, some version history may be missing.
+    </p>
+
     <ng-container *ngIf="vm$ | async as vm; else loading">
-      <div class="d-flex flex-column align-items-start">
-        <h4>
-          {{ vm.interpreterName }} — {{ vm.examType }} — Active Attempt: {{ vm.attempt }}
-        </h4>
-      </div>
-      <div class="d-flex flex-row align-items-baseline">
-        <p class="me-3">Attempt: {{ vm.attempt }}</p>
-        <p class="me-3">Exam Date: {{ vm.scheduledDate | date }}</p>
-        <div class="d-flex flex-row align-items-center">
-          <p class="me-2 mb-0">Version:</p>
-          <select class="form-select" [(ngModel)]="selectedOption">
+      <h4 class="mt-4 mb-3 fw-bold">
+        {{ vm.interpreterName }} — {{ vm.examType }} — Active Attempt: {{ vm.attempt }}
+      </h4>
+
+      <div class="d-flex flex-row mb-4 justify-content-between">
+        <div>
+          <p class="me-3">Attempt: {{ vm.attempt }}</p>
+          <p class="me-3">Exam Date: {{ vm.scheduledDate | date }}</p>
+        </div>
+        <div class="d-flex align-items-center">
+          <label for="version" class="me-2 mb-0">Version:</label>
+          <select id="version" class="form-select" [(ngModel)]="selectedOption">
             <option *ngFor="let option of foo$ | async" [ngValue]="option">
               {{ option.performanceExamVersion }}
             </option>
           </select>
         </div>
       </div>
-      <div class="d-flex mt-3 justify-content-around flex-wrap" *ngIf="saving$ | async as saving">
-        <!-- Add a saving spinner here -->
-        <span class="d-flex align-items-center" *ngIf="saving; else notSaving">
-          <div class="spinner-border text-primary me-2" role="status" style="width: 38px; height: 38px"></div>
-          <span class="visually-hidden">Saving…</span>
-        </span>
-        <ng-template #notSaving>
-          <span>Not Saving...</span>
-        </ng-template>
-        <!-- Use disabled attribute to disable the save button -->
-        <button type="button" (click)="savePerformanceExamsWithSchedule()" class="btn btn-primary me-2" [disabled]="saving">
-          Save
-        </button>
-        <!-- End of saving spinner -->
-        <a routerLink="/dashboard/performance/exams/pending" class="btn btn-secondary">Cancel</a>
+
+      <div class="d-flex justify-content-between align-items-center">
+        <div [style.visibility]="vm.saving ? 'visible' : 'hidden'" class="d-flex align-items-center">
+          <div class="spinner-border text-primary" role="status" style="width: 38px; height: 38px"></div>
+          <span class="ms-2">Saving…</span>
+        </div>
+        <div>
+          <button type="button" (click)="savePerformanceExamsWithSchedule()" class="btn btn-primary btn-lg" [disabled]="vm.saving">
+            Save
+          </button>
+          <a routerLink="/dashboard/performance/exams/pending" class="btn btn-secondary btn-lg ms-2">Cancel</a>
+        </div>
       </div>
     </ng-container>
+
     <ng-template #loading>
       <div class="d-flex justify-content-center">
         <div class="spinner-border text-primary" role="status">
