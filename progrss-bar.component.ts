27e0dd9311a0c83b1b7e890a1a44bac59d68d67f
Jared Whipple <jared.whipple@stgconsulting.com>
2023-06-21 18:19:05 -0600

    Update progress bar

--- progrss-bar.component.ts
+++ progrss-bar.component.ts
@@ -1,37 +1,50 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
-interface Options {
-  color: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info';
-  mode: 'animated' | 'determinate' | 'indeterminate' | 'buffer' | 'query';
+interface ProgressBarOptions {
+  color: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
+  striped: boolean;
+  animated: boolean;
   value: number;
 }
 
 @Component({
-  selector: 'webteam-shared-client-progress-bar',
-  standalone: true,
-  imports: [CommonModule],
+  selector: 'app-progress-bar',
   template: `
-    <div
-      class="progress mt-2"
-      role="progressbar"
-      [ngStyle]="{ visibility: isLoading ? 'visible' : 'hidden' }"
-    >
+    <div class="progress">
       <div
-        class="progress-bar progress-bar-striped progress-bar-animated"
-        style="width: 100%"
+        class="progress-bar"
+        role="progressbar"
+        [class]="getClasses()"
+        [style.width.%]="options.value"
+        aria-valuenow="options.value"
+        aria-valuemin="0"
+        aria-valuemax="100"
       ></div>
     </div>
   `,
-  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StateAwareLoadingIndicatorComponent {
-  @Input() isLoading = false;
-
-  @Input() options: Options = {
+export class ProgressBarComponent {
+  @Input() options: ProgressBarOptions = {
     color: 'primary',
-    mode: 'animated',
-    value: 50,
+    striped: false,
+    animated: false,
+    value: 0,
+  };
+
+  getClasses(): string {
+    let classes = `bg-${this.options.color}`;
+    if (this.options.striped) {
+      classes += ' progress-bar-striped';
+    }
+    if (this.options.animated) {
+      classes += ' progress-bar-animated';
+    }
+    return classes;
   }
 }
+```
+
+In this component, we have an `options` input that allows you to specify the color, whether the progress bar is striped or animated, and the current value of the progress bar. The `getClasses` method is used to dynamically generate the class string for the progress bar based on the options.
+
+Please note that this is a basic example and might not cover all the features of the Bootstrap 5 progress bar. You might need to extend it based on your specific needs.
